a039c39d9788f74317ed50f82a3412630330703a
GitHub Action <action@github.com>
2025-04-15 19:03:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/ankitbanka17/promptbasedcoding/MyBundle.kt
+++ src/main/kotlin/com/github/ankitbanka17/promptbasedcoding/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ankitbanka17.promptbasedcoding
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ankitbanka17/promptbasedcoding/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/ankitbanka17/promptbasedcoding/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.ankitbanka17.promptbasedcoding.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
